23c98b9b173e50a62b0e1714ee2aeba660c3b524
LAPTOP-00D8C9AJ\user <ranggadhiyafeb@gmail.com>
2024-03-18 04:51:12 +0700

    step 4

--- app/src/main/java/com/example/aplikasikotlin/Model/Aquarium.kt
+++ app/src/main/java/com/example/aplikasikotlin/Model/Aquarium.kt
@@ -1,11 +1,31 @@
 package com.example.aplikasikotlin.Model
 
-class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} liters")
+    }
+    
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
     }
-}
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        // calculate the height needed
+        height = (tank / (length * width)).toInt()
+    }
 
+    //Step 4 - Member Variables
+    var volume: Int
+        get() = width * height * length / 1000
+        private set(value) {
+            height = (value * 1000) / (width * length)
+        }
+}
 
