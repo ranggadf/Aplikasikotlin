64e9b8536718cdde0275cd6bc439cf78fcb2e1b2
LAPTOP-00D8C9AJ\user <ranggadhiyafeb@gmail.com>
2024-03-18 07:49:29 +0700

    Lesson3

--- app/src/main/java/com/example/aplikasikotlin/Model/Aquarium.kt
+++ app/src/main/java/com/example/aplikasikotlin/Model/Aquarium.kt
@@ -1,31 +1,24 @@
 package com.example.aplikasikotlin.Model
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
-    init {
-        println("aquarium initializing")
-    }
-    init {
-        // 1 liter = 1000 cm^3
-        println("Volume: ${width * length * height / 1000} liters")
-    }
-    
+
+
+
+open class Aquarium (open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
+    open var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
     fun printSize() {
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
+        // 1 l = 1000 cm^3
+        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
     }
-    constructor(numberOfFish: Int) : this() {
-        // 2,000 cm^3 per fish + extra room so water doesn't spill
-        val tank = numberOfFish * 2000 * 1.1
-        // calculate the height needed
-        height = (tank / (length * width)).toInt()
-    }
-
-    //Step 4 - Member Variables
-    var volume: Int
-        get() = width * height * length / 1000
-        private set(value) {
-            height = (value * 1000) / (width * length)
-        }
 }
-
